0c17041e94506da4fa32ea7c94e6b79e274d7e6b
Rames Kumar <rameshkumar.c.mts@gmail.com>
2021-02-17 00:13:48 +0530

    added billa

--- main.py
+++ main.py
@@ -1,21 +1,34 @@
-import time
+# import pytest
+#
+#
+# #@pytest.mark.smoke
+#
+# def test_firstprogram():
+#     print("hell")
+#
+#
+
+
+import pytest
+from selenium.webdriver.common.keys import Keys
 from selenium import webdriver
-import selenium
-
-driver = webdriver.Chrome(executable_path="C:\Selenium Browser driver\chromedriver.exe")
-driver.get("https://www.rahulshettyacademy.com/AutomationPractice/")
-driver.maximize_window()
-print(driver.title)
-driver.find_element_by_css_selector("input[value='radio2']").click()
-driver.find_element_by_css_selector("option[value='option1']").click()
 
 
+class Practise(object):
+    def test_browser(self):
+        driver = webdriver.Chrome(executable_path="C:\Selenium Browser driver\chromedriver.exe")
+        driver.get("https://www.google.com/")
+        driver.maximize_window()
+        driver.find_element_by_css_selector("input[type='text']").send_keys("myname is billas ")
+        driver.find_element_by_css_selector("input[type='text']").send_keys(Keys.ENTER)
+
+        print(driver.title)
 
 
-driver.find_elements_by_id("")
+run = Practise()
+run.test_browser()
 
-
-
-
+def test_demo():
+    print("My name is Billa")
 
 
