cded432d498e3d72a21f04a7fe3b3fde35234902
crk007python <67868591+crk007python@users.noreply.github.com>
2022-09-04 01:05:54 +0530

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -1,32 +1,30 @@
-# import pytest
-#
-#
-# #@pytest.mark.smoke
-#
-# def test_firstprogram():
-#     print("hell")
-#
-#
-
-
-import pytest
-from selenium.webdriver.common.keys import Keys
 from selenium import webdriver
+import time
 
-
-class Practise(object):
-    def test_browser(self):
+class alerrt:
+    def alertpopu():
         driver = webdriver.Chrome(executable_path="C:\Selenium Browser driver\chromedriver.exe")
-        driver.get("https://www.google.com/")
-        driver.maximize_window()
-        driver.find_element_by_css_selector("input[type='text']").send_keys("myname is billaaa ")
-        driver.find_element_by_css_selector("input[type='text']").send_keys(Keys.ENTER)
+        driver.get("https://matches.tamilmatrimony.com/myhome")
+        driver.find_element_by_id('MIDP').send_keys("rameshkumar.c.mts@gmail.com")
+        driver.find_element_by_id('PASSWORD2').send_keys("kalaikathir")
+        driver.find_element_by_xpath('//*[@value="LOGIN"]').click()
+        driver.find_element_by_xpath('//*[contains(text(),"M7265703")]').click()
+        time.sleep(6)
+        driver.get("https://profile.tamilmatrimony.com/profiledetail/myprofile.php")
+        driver.find_element_by_xpath('//*[@id="abotmembermenulink"]').click()
+        time.sleep(6)
+        # to find insta id.........
+        txt = []
+        txt = driver.find_element_by_xpath('//*[@id="DESCRIPTION1"]').text
+        print(txt)
+        text = txt.find("c.r.k.007")
+        print(text)
+        # if insta id is not present --it return -1..so it execuete below code
+        if text == -1:
+            driver.find_element_by_xpath('//*[@id="DESCRIPTION1"]').send_keys(" insta-c.r.k.007")
+            driver.find_element_by_xpath('//*[@id="prdescsave"]').click()
+        time.sleep(6)
+        driver.close()
 
-        print(driver.title)
-
-
-run = Practise()
-run.test_browser()
-
-
-
+obj = alerrt
+obj.alertpopu()
